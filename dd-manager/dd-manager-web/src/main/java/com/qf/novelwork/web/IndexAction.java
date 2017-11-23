94de207f0d11756fec5279fd7d27f1c15231d4ec
wffsomethingfornothing <1034646483@qq.com>
2017-11-23 19:09:58 +0800

    主页姓名，用户退出

--- dd-manager/dd-manager-web/src/main/java/com/qf/novelwork/web/IndexAction.java
+++ dd-manager/dd-manager-web/src/main/java/com/qf/novelwork/web/IndexAction.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by ${lxh} on 2017/11/13 0013
@@ -48,12 +49,12 @@ public class IndexAction {
 
     @RequestMapping("/checkname")
     @ResponseBody
-    public String doLogin(NAdmin nAdmin) throws Exception{
+    public String doLogin(NAdmin nAdmin, HttpSession session) throws Exception{
         String  mess = "";
         NAdmin findUser = nAdminService.selectByadminname(nAdmin);
         if(findUser!=null){
             if(findUser.getPassword().equals(nAdmin.getPassword())){
-                        /*session.setAttribute("sessionUser", findUser);*/
+                        session.setAttribute("sessionAdmin", findUser);
                 mess="2";
             }else{
                 mess="3";
@@ -63,6 +64,9 @@ public class IndexAction {
         }
         return mess;
     }
-
-
+    @RequestMapping("/logout")
+    public String doLogout(HttpSession session){
+        session.removeAttribute("sessionAdmin");
+        return "login";
+    }
 }
